9f9ad36fbfdec62e9dc9c51ed3eb390e56df15b1
Julian Weisbord Desktop <julianweisbord@gmail.com>
2015-06-18 19:54:42 -0700

    adding files and creating games while

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,1 +1,13 @@
 import sys
+import tool
+import RPSGame
+
+answer = 'y'
+
+while(answer == 'y'):
+	#function call
+
+	answer = raw_input("Would you like to play again(y or n)?")
+	if(answer!='y'):
+		print("exited")
+		sys.exit(0)
