cb7d4fa51c8978222df17d3291e63bc56c852942
Julian Weisbord Desktop <julianweisbord@gmail.com>
2015-06-18 19:50:02 -0700

    import files and create games while

---
+++ RPSGame.py
@@ -0,0 +1,3 @@
+import tool
+
+#class RPSGame:
---
+++ tool.py
@@ -0,0 +1,10 @@
+
+class Tool:
+	def get_type(self):
+		return type
+
+	def set_type(self, human_choice):
+		type = human_choice
+
+#class Rock:		
+		
